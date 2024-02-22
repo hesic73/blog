94f206d797cd39dec83a248d9a6f9048041b8977
hesic73 <hesicheng2001@163.com>
2024-02-22 01:20:54 -0800

    temporarily save the draft

--- new_post.cmd
+++
@@ -1,8 +0,0 @@
-@echo on
-REM This script executes post_config.exe with specified arguments
-
-REM Change the arguments below as needed
-chcp 65001
-post_config.exe 2023年总结（4）^
-  --output-dir _posts ^
-  --categories 总结 ^
